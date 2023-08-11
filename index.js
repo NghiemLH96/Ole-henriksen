6b69e0b4d44927eb103fc5c2f372f14b55d55387
NghiemLH96 <isaacisc01022014@gmail.com>
2023-08-11 13:18:46 +0700

    OleHenriksen

--- index.js
+++ index.js
@@ -2,45 +2,116 @@
 
 let productsList = JSON.parse(localStorage.getItem("productsList")) ?? [];
 console.log(productsList);
-//render Product
-let renderInner = "";
-let innerTotal=0;
-function renderProduct(id) {
-    
-    for (let i = 0; i < productsList.length; i++) {
+//pagination
+let itemsPerPage = 32;
+let totalPages = Math.ceil(productsList.length / itemsPerPage)
 
-        renderInner +=
+//render page button
+function renderPage() {
+    let pageInner = "";
+    for (let i = 0; i < totalPages; i++) {
+        pageInner+=`<li onclick=activePage(${i+1}) class="pagination__pageNumber">${i+1}</li>`
+    }
+    document.getElementById("producList__pagination__page").innerHTML=pageInner
+}
+renderPage()
+//active Page highlight
+let currentPage=1;
+let start;
+let end;
+function activePage(active) {
+    currentPage=active;
+    let pages=document.getElementsByClassName("pagination__pageNumber");
+    for (let i = 0; i < pages.length; i++) {
+        if(i==active-1){
+            pages[i].classList.add("active__page");
+        }else{
+            pages[i].classList.remove("active__page");
+        }
+    }
+    startEnd(active)
+    //if active = 1 hidden previous button
+    if (currentPage==1) {
+        document.getElementById("producList__pagination__pre-btn").style.visibility="hidden"
+    }else{
+        document.getElementById("producList__pagination__pre-btn").style.visibility="visible"
+    }
+    //if active = totalPages hidden next button
+    if (currentPage==totalPages) {
+        document.getElementById("producList__pagination__next-btn").style.visibility="hidden"
+    }else{
+        document.getElementById("producList__pagination__next-btn").style.visibility="visible"
+    }
+    window.scrollTo({
+        top:0,
+        behavior:`smooth`
+    })
+}
+
+//caculator render products index
+function startEnd(current) {
+    start=(current-1)*itemsPerPage;
+    end=current*itemsPerPage;
+    renderProduct(productsList)
+    console.log(start);
+    console.log(end);
+}
+
+activePage(1)
+
+//previous page button
+function previousPage(){
+        currentPage=--currentPage;
+        activePage(currentPage);
+        renderProduct(productsList);
+}
+
+function nextPage(){
+    currentPage=++currentPage;
+    activePage(currentPage);
+    renderProduct(productsList);
+}
+
+
+//render Product
+function renderProduct(arrProducts) {
+    let renderInner = "";
+    let innerTotal = 0;
+    for (let i = 0; i < arrProducts.length; i++) {
+        if(i>=start && i<end){
+            innerTotal++
+            renderInner+=
             `
             <div class="product">
                 <div class="imgContainer">
-                    <img id="img${productsList[i].id[0]}" class="product__Img" src="${productsList[i].productImg[0]}" alt="">
-                    <img id="img${productsList[i].id[1]}" class="product__Img display" src="${productsList[i].productImg[1]}" alt="">
+                    <img id="img${arrProducts[i].id[0]}" class="product__Img" src="${arrProducts[i].productImg[0]}" alt="">
+                    <img id="img${arrProducts[i].id[1]}" class="product__Img display" src="${arrProducts[i].productImg[1]}" alt="">
                 </div>
                 <div class="detailBox">
-                    <p class="product__bestSellerText">${productsList[i].introduceText}</p>
-                    <p class="product__name">${productsList[i].productName}</p>
+                    <p class="product__bestSellerText">${arrProducts[i].introduceText}</p>
+                    <p class="product__name">${arrProducts[i].productName}</p>
                 </div>
                 <div class="buttonBox">
-                    <div class="sizeButtonBox__container">${sizeButtonCreate(productsList[i])}</div>
-                    <button class="statusButtonBox">${checkProductStatus(productsList[i])}</button>
+                    <div class="sizeButtonBox__container">${sizeButtonCreate(arrProducts[i])}</div>
+                    <button class="statusButtonBox">${checkProductStatus(arrProducts[i])}</button>
                 </div>
             </div>
-        `
-        innerTotal=i+1
+            `
+        }
     }
-    document.getElementById("total").innerText=`${innerTotal} PRODUCTS`
+    document.getElementById("total").innerText = `${innerTotal} PRODUCTS`
     document.getElementById("productContainer").innerHTML = renderInner;
 }
 renderProduct(productsList.id)
 
 
 function checkProductStatus(Product) {
-    let addToBagButtonInner="";
+    let addToBagButtonInner = "";
     let productStatus = Product.status;
-    
-    if(productStatus=="Available"){
-        addToBagButtonInner+=
-        `
+
+    if (productStatus == "Available") {
+        addToBagButtonInner +=
+            `
         <div class="addToBag">
             <p>Add To Bag</p>
             <div class="priceContainer">
@@ -51,18 +122,18 @@ function checkProductStatus(Product) {
             </div>
         </div>
         `
-        return innerHTML=addToBagButtonInner;
-    }if (productStatus=="Notify Me") {
-        addToBagButtonInner+=
-        `
+        return innerHTML = addToBagButtonInner;
+    } if (productStatus == "Notify Me") {
+        addToBagButtonInner +=
+            `
         <div class="Notify-Me">
             <p>Notify Me</p>
         </div>
         `
-        return innerHTML=addToBagButtonInner;
-    }if (productStatus=="Full Detail") {
-        addToBagButtonInner+=
-        `
+        return innerHTML = addToBagButtonInner;
+    } if (productStatus == "Full Detail") {
+        addToBagButtonInner +=
+            `
         <div id="seeFullDetail${Product.id}" class="addToBag">
             <p>See Full Detail</p>
             <div class="priceContainer">
@@ -74,15 +145,15 @@ function checkProductStatus(Product) {
             </div>
         </div>
         `
-        return innerHTML=addToBagButtonInner;
-    }if (productStatus=="Sold Out") {
-        addToBagButtonInner+=
-        `
+        return innerHTML = addToBagButtonInner;
+    } if (productStatus == "Sold Out") {
+        addToBagButtonInner +=
+            `
         <div class="Notify-Me">
             <p>Sold Out</p>
         </div>
         `
-        return innerHTML=addToBagButtonInner;
+        return innerHTML = addToBagButtonInner;
     }
 }
 
@@ -103,63 +174,62 @@ function sizeButtonCreate(Product) {
        `
         }
         return innerHTML = sizeButtonInner;
-    } else if(productSize.length == 0) {
-        return innerHTML=""
+    } else if (productSize.length == 0) {
+        return innerHTML = ""
     }
 }
 
-function changeDetailbySize(id){
+function changeDetailbySize(id) {
     console.log(id);
     for (let i = 0; i < productsList.length; i++) {
-        if(productsList[i].id.length>=2){
-            let count=0;
+        if (productsList[i].id.length >= 2) {
+            let count = 0;
             for (let j = 0; j < productsList[i].id.length; j++) {
                 /* document.getElementById(`img${productsList[i].id[j]}`).classList.add("display");
                 document.getElementById(`price${productsList[i].id[j]}`).classList.add("display"); */
-                if(productsList[i].id[j]==id){
-                    let idIndex=count;
-                    if (idIndex==0) {
-                        document.getElementById(`img${productsList[i].id[idIndex+1]}`).classList.add("display");
-                        document.getElementById(`price${productsList[i].id[idIndex+1]}`).classList.add("display");
+                if (productsList[i].id[j] == id) {
+                    let idIndex = count;
+                    if (idIndex == 0) {
+                        document.getElementById(`img${productsList[i].id[idIndex + 1]}`).classList.add("display");
+                        document.getElementById(`price${productsList[i].id[idIndex + 1]}`).classList.add("display");
                         document.getElementById(`img${productsList[i].id[idIndex]}`).classList.remove("display");
                         document.getElementById(`price${productsList[i].id[idIndex]}`).classList.remove("display");
-                    }if (idIndex==1) {
-                        document.getElementById(`img${productsList[i].id[idIndex-1]}`).classList.add("display");
-                        document.getElementById(`price${productsList[i].id[idIndex-1]}`).classList.add("display");
+                    } if (idIndex == 1) {
+                        document.getElementById(`img${productsList[i].id[idIndex - 1]}`).classList.add("display");
+                        document.getElementById(`price${productsList[i].id[idIndex - 1]}`).classList.add("display");
                         document.getElementById(`img${productsList[i].id[idIndex]}`).classList.remove("display");
                         document.getElementById(`price${productsList[i].id[idIndex]}`).classList.remove("display");
                     }
-                    count=0;
+                    count = 0;
                 }
                 count++
             }
-        }      
+        }
     }
-} 
+}
 
-function logout(){
+function logout() {
     localStorage.removeItem("CheckLogin")
 }
 
 let usersList = JSON.parse(localStorage.getItem("usersList"));
 
 function checkLogin() {
-    let nameDisplay=document.getElementById("userDetailName");
-    let avatarDisplay=document.getElementById("userDetailImg");
+    let nameDisplay = document.getElementById("userDetailName");
+    let avatarDisplay = document.getElementById("userDetailImg");
     if (localStorage.getItem("CheckLogin")) {
-        console.log("11111");
-        let CheckLogin= localStorage.getItem("CheckLogin");
-        document.getElementById("login_icon").style.display="none";
-        document.getElementById("logout_icon").style.display="block";
-        nameDisplay.style.display="block";
-        nameDisplay.innerText= (usersList.find(item => item.id == CheckLogin).fistName) + (usersList.find(item => item.id == CheckLogin).lastName);
-        avatarDisplay.style.display="block"
-        avatarDisplay.src= usersList.find(item => item.id == CheckLogin).avatar;
-    }else{
-        document.getElementById("login_icon").style.display="block";
-        document.getElementById("logout_icon").style.display="none";
-        document.getElementById("userDetailName").style.display="none";
-        document.getElementById("userDetailImg").style.display="none";
+        let CheckLogin = localStorage.getItem("CheckLogin");
+        document.getElementById("login_icon").style.display = "none";
+        document.getElementById("logout_icon").style.display = "block";
+        nameDisplay.style.display = "block";
+        nameDisplay.innerText = (usersList.find(item => item.id == CheckLogin).fistName) + (usersList.find(item => item.id == CheckLogin).lastName);
+        avatarDisplay.style.display = "block"
+        avatarDisplay.src = usersList.find(item => item.id == CheckLogin).avatar;
+    } else {
+        document.getElementById("login_icon").style.display = "block";
+        document.getElementById("logout_icon").style.display = "none";
+        document.getElementById("userDetailName").style.display = "none";
+        document.getElementById("userDetailImg").style.display = "none";
     }
 
 }
